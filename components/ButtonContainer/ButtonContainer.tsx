e3eb9b298f8b4d62891d6466e154d4a72af96ac4
Dima Shirkov <dimashirkov@list.ru>
2024-05-09 23:39:16 +0300

    add console.log

--- components/ButtonContainer/ButtonContainer.tsx
+++ components/ButtonContainer/ButtonContainer.tsx
@@ -3,6 +3,7 @@ import styles from "./ButtonContainer.module.scss"
 import cn from 'classnames';
 
 export const ButtonContainer = ({children}:ButtonContainerProps) => {
+    console.log('hi')
     return(
         <div className={styles.buttonContainer}>
             {children}
